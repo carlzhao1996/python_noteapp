eab797e5cacaf6203da63a47f5dab0c408f5448a
CarlZhao <carl1996@outlook.com>
2018-03-20 12:11:31 +1300

    commit

---
+++ noteapp/views/createnote.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, request, redirect
+import random
+
+bp = Blueprint(__name__,__name__,template_folder='templates')
+
+def random_id(length=16):
+    final_string = ''
+    chars = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+    for i in range(0, length):
+	final_string += chars[random.randint(0, len(chars)-1)]
+    return final_string
+
+@bp.route('/createnote', methods=['POST', 'GET'])
+def show():
+    if request.method == 'POST':
+	if request.form.get('createnote'):
+	    text = request.form.get('notetext')
+	    
+	    with open('noteapp/notes/{}.note'.format(random_id()), 'w+') as _file:
+	      _file.write(text)
+		
+	    _file.close()
+	    return redirect('/')		
+
+	   	
+    return render_template('createnote.html')
